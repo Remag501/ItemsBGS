217d9edbf36ef09e7841e278104cc25b35695d40
Remag501-Desktop <evanverma2003@gmail.com>
2025-10-11 11:27:41 -0700

    Initial Commit

---
+++ src/main/java/org/example/Remag501/bGSItems/BGSItems.java
@@ -0,0 +1,17 @@
+package org.example.Remag501.bGSItems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BGSItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
